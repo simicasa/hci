047f28dd0ba6299d1715d61936a52637272031a2
simone <simone.casaburi@gmail.com>
2015-11-12 14:34:33 +0100

    prova

--- app/Http/Controllers/GestioneImmagini.php
+++ app/Http/Controllers/GestioneImmagini.php
@@ -72,7 +72,7 @@ class GestioneImmagini extends Controller
 
     public function GetImageFromApp(Request $req){
         $id = $req->input('id');
-        $elem = json_encode(Immagini::select('Immagine','Testo','DataFoto')->where('id_marker','=',$id)->orderby("DataFoto")->get());
+        $elem = json_encode(Immagini::select('Immagine','Testo','DataFoto')->where('id_marker','=',$id)->orderby("DataFoto","desc")->get());
         return $elem;
     }
     /**
